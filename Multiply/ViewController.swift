8e1e9ac5bac5b025ec5fecf9df34f38ac3c2a9c6
cstark <student@cstark.local>
2016-01-08 11:16:10 -0600

    initial commit

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myFirstTextField: UITextField!
+    @IBOutlet weak var mySecondTextField: UITextField!
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var SegController: UISegmentedControl!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func myButton(sender: AnyObject)
+    {
+        let myFirstTextFieldString = myFirstTextField.text
+        let myFirstTextFieldDouble = Double(myFirstTextFieldString!)
+        let mySecondTextFieldString = mySecondTextField.text
+        let mySecondTextFieldDouble = Double(mySecondTextFieldString!)
+        let index = SegController.selectedSegmentIndex
+        var answer : Double!
+        switch index
+        {
+        case 0:
+            answer = myFirstTextFieldDouble! + mySecondTextFieldDouble!
+            break
+            
+        case 1:
+            answer = myFirstTextFieldDouble! - mySecondTextFieldDouble!
+            break
+            
+        case 2:
+            answer = myFirstTextFieldDouble! * mySecondTextFieldDouble!
+            break
+            
+        case 3:
+            answer = myFirstTextFieldDouble! / mySecondTextFieldDouble!
+            break
+            
+        default:
+            break
+            
+        }
+        myLabel.text = "\(answer)"
+        if (answer == 64)
+        {
+            myImageView.image = UIImage(named: "myMKImage")}
+        else if answer % 2 == 1
+        {
+            myImageView.image = UIImage(named:"CookieMonsterWaving")
+        }
+        else if answer % 2 == 0
+        {
+            myImageView.image = UIImage(named:"funnyPic")
+        }
+        myFirstTextField.resignFirstResponder()
+        mySecondTextField.resignFirstResponder()
     }
-
-
 }
-
